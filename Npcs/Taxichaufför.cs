4d725a7635cc51e39e38b923bf14129fa97fd04b
Aldo651A <aldito43ariasg@hotmail.se>
2026-09-24 23:45:24 +0200

    Har gjort om programmet till ett mer rak struktur med if , else if och else slipper inestlade if satser. Blir lättare att läsa programmet.

--- Npcs/Taxichaufför.cs
+++ Npcs/Taxichaufför.cs
@@ -9,6 +9,7 @@ class TaxiDriver : Npc
     
     public override void Interact(Player player)
     {
+        
         Menu taxiMenu = new Menu();
         int adressen = taxiMenu.Ask("Hoppa in! Har du en adress du ska till? ",
         ["Ja", "Nej"]);
@@ -19,45 +20,61 @@ class TaxiDriver : Npc
             Console.WriteLine("\"Jag kan tyvärr inte köra dig\"");
             player.GameOver = true;
         }
+        else if (player.Backpack.Has("adressen") && player.Backpack.Has("taxikort"))
+        {
+            player.Backpack.Remove("adressen");
+            player.Backpack.Remove("taxikort");
+            Taxiresa = true;
+            Console.WriteLine("\"You are Good To Go.\"");
+            return;
+       
+        }   
+        else if (player.Backpack.Has("adressen") && !player.Backpack.Has("taxikort"))
+        {
+            Console.WriteLine("\"Sorry grabben! Du saknar taxikortet! Kan tyvärr inte köra dig..\"");
+        }
         else
         {
-             if (player.Backpack.Has("adressen") && player.Backpack.Has("taxikort"))
-            {
-                player.Backpack.Remove("adressen");
-                player.Backpack.Remove("taxikort");
-                Taxiresa = true;
-                Console.WriteLine("\"You are Good To Go.\"");
-                return;
-            }
-            else 
-            {
-                Console.WriteLine("\"Du saknar antigen adressen eller taxikor.\" ");
-            
-                int bönfall = taxiMenu.Ask("Snälla chauffören! min brud väntar på mig i någon kyrka i malmö. Kan du inte bara köra mig till närmaste kyrka...",
-                ["Böna och be", "Ge upp" ]);
-
-                if (bönfall == 1)
-                {
-                    Console.WriteLine("\"Ok! Grabben vi kör dig till brölpppet. Vart det nu än är? \"");
-                    Console.WriteLine("\"Hoppas vi hittar rätt kyrka.\"");
-                    Taxiresa = true;
-                
-                    if (player.Backpack.Has("taxikort"))
-                    {
-                        player.Backpack.Remove("taxikort");
-                    }
-                
-                }
-                else 
-                {
-                    Console.WriteLine("\"Utan adress kan tyvärr inte köra dig.\"");
-                }
-            
-            }   
-        
+            Console.WriteLine("\"Försök och hitta kortet! Jag kan vänta.\"");
         }
-    
+        
+        
+        int bönfall = taxiMenu.Ask("Snälla chauffören! Jag ska gifta mig och min brud väntar på mig i någon kyrka i malmö. Kan du inte bara köra mig till närmaste 10 kyrkor...",
+        ["Böna och be", "Ge upp" ]);
+
+        if (bönfall == 1)
+        {
+            Console.WriteLine("\"Ok! Grabben vi kör dig till brölpppet. Vart det nu än är? \"");
+            Console.WriteLine("\"Hoppas vi hittar rätt kyrka.\"");
+            Taxiresa = true;
+        }
+        else if (player.Backpack.Has("taxikort"))
+        {
+            player.Backpack.Remove("taxikort");
+        }
+        else
+        {
+            Console.WriteLine("\"Har du ingen adress kan jag tyvärr inte köra dig.\"");
+        }
+
+
+           
+            
+                
     }
 
+}       
 
-}
+
+      
+    
+
+
+      
+              
+        
+        
+    
+    
+
+
