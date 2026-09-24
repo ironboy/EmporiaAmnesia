ec8aa16ec59e8f0a52e844a853fa4ad38420039c
Danial Karezi <danialkarezi2000@gmail.com>
2026-09-24 21:49:02 +0200

    Update roof unlock description

--- Locations/Roof.cs
+++ Locations/Roof.cs
@@ -4,7 +4,7 @@ class Roof : Location
     {
         //Set the location name and description
         Name = "Taket";
-        Description = "Du står vid dörren till taket. Dörren är låst och kräver ett nykelkort.";
+        Description = "Du står vid dörren till taket. Dörren är låst och kräver ett nyckelkort.";
         //Kepp the way back open, but lock they way to the parking deck
         Directions = [Direction.West];
     }
@@ -16,6 +16,8 @@ class Roof : Location
             Console.WriteLine("Du håller nyckelkortet mot läsaren. Dörren till taket låses upp.");
             //Restore all available driections after unlocking the door
             Directions = Map.DirectionsFor(this);
+
+            Description = "Dörren till taket är upplåst. Österut ligger parkeringsdäcket.";
         }
         else
         {
