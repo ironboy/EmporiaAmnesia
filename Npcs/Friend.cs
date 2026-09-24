06ad17caf116feb8d076d13108d96a0c7bf537f4
Mr-Christoffer <ctsa.nilsson@gmail.com>
2026-09-24 14:39:11 +0200

    Friend progress

--- Npcs/Friend.cs
+++ Npcs/Friend.cs
@@ -1,24 +1,40 @@
 class Friend : Npc
 {
-  private bool _cleanerTalk = false;
-  public bool FindFriend = false;
+    private bool talkToFriend = true;
+    public bool FindFriend = false;
 
-  public Friend()
-  {
-    Name = "Vän";
-  }
+    public Friend()
+    {
+        Name = "Vän";
+    }
 
-  public override void Interact(Player player)
-  {
-    bool friendInt = true;
-    while (friendInt)
+    public override void Interact(Player player)
+    {
+        bool friendInt = true;
+        
+        while (friendInt)
         {
-            System.Console.WriteLine("Din vän ligger utslagen på golvet. Vad vill du göra?");
-            Menu friendMenu = new Menu();
-            int chosen = friendMenu.Ask(
-            "Om du har pengar skulle vi kunna prata om en lösning...",
-            ["Ja", "Nej"]
-        );
+            System.Console.WriteLine("Din vän ligger utslagen på golvet. ");
+            if (talkToFriend == true)
+            {
+                Menu friendMenu = new Menu();
+                int chosen = friendMenu.Ask(
+           "Vad vill du göra?..",
+           ["Lavetta honom", "Häll vatten på honom"]);
+                switch (chosen)
+                {
+                    case 1:
+                        System.Console.WriteLine("Ugghhhh..");
+                        break;
+                    case 2:
+                        System.Console.WriteLine("Sluta...");
+                        break;
+                }
+            }
         }
-  }
+    }
 }
+
+
+
+
