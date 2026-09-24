bf22b330d7edf2da9ad5a168a68cae80f10a773a
Aldo651A <aldito43ariasg@hotmail.se>
2026-09-24 20:16:22 +0200

    har uppdaterat program har gjort att man kan kontrollera både adressen och taxikort på samma gång istället för att göra det på två Menu med två olika if satser.  Jag tycker att det blir enklare så här.

--- Npcs/Taxichaufför.cs
+++ Npcs/Taxichaufför.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 class TaxiDriver : Npc
 {
     public bool Taxiresa = false;
@@ -9,38 +11,33 @@ class TaxiDriver : Npc
     
     public override void Interact(Player player)
     {
-
-        if (player.Backpack.Has("adressen") && player.Backpack.Has("taxikort"))
-        {
-            Console.WriteLine("\"You are Good To Go.\"");
-            return;
-        }
-
         Menu taxiMenu = new Menu();
+        int adressen =taxiMenu.Ask("Hoppa in! Har du en adress du ska till? ",
+        ["Ja", "Nej"]);
 
-        // Taxichauffören kontrollerar om player har en adress
-        int adress = taxiMenu.Ask("Hoppa in! Har du en adressen du ska till?",
-        ["Ja, jag har adressen", "Nej, jag har inte adressen"]);
-
-        if (adress == 2 /*Nej, jag har ingen adress*/)
+        if (adressen == 2)
         {
-            Console.WriteLine("\"Om du inta har någon adress! Så kan jag inte köra dig. ");
-            Console.WriteLine("\"Men jag väntar till du hittar adressen");
+            Console.WriteLine("\"Har du ingen adress!\"");
+            Console.WriteLine("\"Jag kan tyvärr inte köra dig\"");
             player.GameOver = true;
         }
-        else /*Ja, jag har en adress*/
+        else
         {
-            if (player.Backpack.Has("adressen"))
+             if (player.Backpack.Has("adressen") && player.Backpack.Has("taxikort"))
             {
                 player.Backpack.Remove("adressen");
+                player.Backpack.Remove("taxikort");
                 Taxiresa = true;
-                Console.WriteLine("Taxichauffören startar bilem och kör. ");
+                Console.WriteLine("\"You are Good To Go.\"");
+                return;
+            }
+            else 
+            {
+                Console.WriteLine("\"Du saknar antigen adressen eller taxikor.\" ");
             }
         
-        
         }
-
-
+    
     
     }
 
