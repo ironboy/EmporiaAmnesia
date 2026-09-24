ec2e1eb3a2e2de04ca44797b313084c899e63efe
Danial Karezi <danialkarezi2000@gmail.com>
2026-09-24 22:00:07 +0200

    Fix roof comments

--- Locations/Roof.cs
+++ Locations/Roof.cs
@@ -2,19 +2,19 @@ class Roof : Location
 {
     public Roof()
     {
-        //Set the location name and description
+        // Set the location name and description
         Name = "Taket";
         Description = "Du står vid dörren till taket. Dörren är låst och kräver ett nyckelkort.";
-        //Kepp the way back open, but lock they way to the parking deck
+        // Keep the way back open, but lock the way to the parking deck
         Directions = [Direction.West];
     }
     public override void Interact(Player player)
     {
-        //Check if the plyaer has the required keycard
+        // Check if the player has the required keycard
         if (player.Backpack.Has("nyckelkort"))
         {
             Console.WriteLine("Du håller nyckelkortet mot läsaren. Dörren till taket låses upp.");
-            //Restore all available driections after unlocking the door
+            // Restore all available directions after unlocking the door
             Directions = Map.DirectionsFor(this);
 
             Description = "Dörren till taket är upplåst. Österut ligger parkeringsdäcket.";
