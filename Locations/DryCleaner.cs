238b0b068f616481a9be19da8a2ad9ec737e5d67
Emelianna Axelsson <emelianna.axelsson@gmail.com>
2026-09-24 10:40:57 +0200

    Skrivit lite under name och description i Locations - DryCleaner

--- Locations/DryCleaner.cs
+++ Locations/DryCleaner.cs
@@ -1,4 +1,12 @@
 class DryCleaner : Location
 {
+ public DryCleaner()
+  {
+    Name = "Kemtvätt";
+    Description = "Jag står inne på kemtvätten. I handen har jag kvittot för att hämta ut min kostym";
+    //Ska spelaren interagera med den anställde i kemtvätten för att få sin kostym, eller sker det direkt i Description?
+  }
+
+//Sker det någon Interact här med rummet, eller bara med personalen i klassen npcs DryCleaner?
 
 }
