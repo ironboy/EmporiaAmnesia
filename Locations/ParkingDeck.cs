9fd1c7f11de09760b8b2e75f742b1e2f5aa846d2
Danial Karezi <danialkarezi2000@gmail.com>
2026-09-25 13:11:07 +0200

    Complete parking deck and mother in law

--- Locations/ParkingDeck.cs
+++ Locations/ParkingDeck.cs
@@ -1,4 +1,14 @@
 class ParkingDeck : Location
 {
+    private MotherInLaw _motherInLaw = new();
+    public ParkingDeck()
+    {
+        Name = "Parkeringsdäcket";
+        Description = "Det blåser över parkeringsdäcket. Din blivande svärmor står en bit bort och röker.";
+    }
+    public override void Interact(Player player)
+    {
+        _motherInLaw.Interact(player);
+    }
 
 }
