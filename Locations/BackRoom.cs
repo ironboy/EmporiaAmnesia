a491d2fd8a04caae182b7e927354eb01f58a3bfe
talha <talhamalik8934@gmail.com>
2026-09-25 12:09:44 +0200

    Lagt till Kodkommentar för Backrooms fil.

--- Locations/BackRoom.cs
+++ Locations/BackRoom.cs
@@ -4,6 +4,8 @@ public BackRoom()
   {
     Name = "Bakrummet";
     Description = "Jag har kommit in i det lilla rummet bakom kemtvätten. Det är saker överallt. En hög med kläder, en tvättkorg, en liten skål med mynt, knappar och andra småsaker. ";
+    
+    // Lägger till föremål som spelaren kan undersöka och ta med sig
     Items.Add(new Item("Smutstvätt", "Kanske något som passar till kostymen?."));
     Items.Add(new Item("En påse med jordnötsringar", "Kan funka som vigselring i nödfall? ."));
   }
@@ -13,22 +15,26 @@ public BackRoom()
         
         bool foundRing = false;
 
+        // Snurrar runt tills man hittar ringen eller ger upp (break).
         while (!foundRing) 
         {
 
         Console.Write("Var vill du leta? 1. Under klädhögen, 2. I tvättkorgen, 3. I skålen \n4. Strunta att leta efter vigselringen och ta en av jordnötsringarna i påsen på bordet istället ");
         string? chosenPlace = Console.ReadLine();
 
+        // Fel ställe.
         if (chosenPlace == "1")
         {
             Console.WriteLine("Här finns ingenting. Bara kläder.");
             
         }
+        // Fel ställe.
        else if (chosenPlace == "2")
         {
             Console.WriteLine("Här finns ingenting. Bara en massa smutsiga kläder.");
             
         }
+        // Rätt ställe.
         else if (chosenPlace == "3")
         {
             Console.WriteLine(@"
@@ -37,18 +43,20 @@ public BackRoom()
 |     |
  \___/ 
 ");
+            // Måste stavas med litet "r" enligt uppgiften.
             Console.WriteLine("Åh en sån lättnad! Här är min ring!!");
             player.Backpack.Add(new Item("Ring", "Vigselringen till bröllopet"));
-            foundRing = true;
+            foundRing = true; // Detta gör att loopen avslutas.
         
-        {
+        
         }
-        }
+
+        // Spelaren ger upp och tar en jordnötsring.
         else if (chosenPlace == "4")
             {
                 player.Backpack.Add(new Item("Jordnötsring", "Ring som ring?"));
                 Console.WriteLine("Hoppas den passar!");
-                break;
+                break; // "break" avbryter loopen direkt, även om foundRing är false.
             }
         }
     }
